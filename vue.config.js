f1c8e429245cdb8dbdec1d2e0823e3843ec4b714
dyxyz <1248716629@qq.com>
2020-09-19 19:58:43 +0800

    dy

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/vue',
-  outputDir: '/dev/tomcat8/webapps/ROOT/vue',
+  outputDir: 'D:/vue',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
@@ -42,7 +42,8 @@ module.exports = {
 
       [process.env.VUE_APP_BASE_API]: {
         ws: false,
-        target: `http://localhost:8080/func/`,
+        // target: `http://localhost:8080/func/`,
+        target: `http://144.217.157.53:9080/func/`,
         changeOrigin: true,
         pathRewrite: {
           [process.env.VUE_APP_BASE_API]: '/'
