66a15af7e5c1c3d04df5cfde7095d4f88ef977fd
lixueqingsd <xqli@sdu.edu.cn>
2020-10-04 17:04:52 +0800

    lxq

--- src/api/globalVariable.js
+++ src/api/globalVariable.js
@@ -1,5 +1,5 @@
-//const servicePort = 'http://144.217.157.53:9080'
-const servicePort = 'http://localhost:8080'
+const servicePort = 'http://144.217.157.53:9080'
+//const servicePort = 'http://localhost:8080'
 
 
 export default {
